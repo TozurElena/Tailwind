939d51dea8ab517efd4c031c0dd9e6ea00f4adb8
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-06-28 10:57:47 +0200

    terminer section hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,9 @@ module.exports = {
         darked: '#D70026',
         lightgray: '#747474',
         darkgray: '#272727'
+      },
+      boxShadow: {
+        'customred': '0px 0px 30px rgba(255, 13, 56, 0.21)'
       }
     },
   },
